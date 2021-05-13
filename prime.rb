39e721d4e1530363191f2fe72dba09360c3c852f
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-13 19:21:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,4 @@
 def prime?(num)
-  n=1
-  if num <= 1
-    return false
-  end
-  if num == 2
-    return true
-  end
-  while n < num
-    if num % num == 0
-      return true
-    else 
-      return false
-    n += 1
-    end
-  end
+  if num.
+  
 end
