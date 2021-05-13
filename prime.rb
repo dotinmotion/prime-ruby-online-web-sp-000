ae5abf3f47a0c6f96365d3ab46d378ae99326a8b
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-13 19:36:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,5 @@
 def prime?(num)
-  if num.even?
-    return false
-  elsif (2..Float::INFINITY).include?(num) && num % 1 == 0
+  if (2..Float::INFINITY).include?(num) && num % 1 == 0 
     return true
   else
     return false
