e07800e3ed8996bf611def0275105744a0f5ad0b
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-13 19:48:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,12 @@ def prime?(num)
     return false
   elsif num == 2 
     return true
-  else
+  elsif
     (2..value - 1).each do |i|
       if value % i == 0
-      return false
+        return false
+      end
+    end
   end
   true
 end
