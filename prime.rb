1c4c94478b296b1ca851dc0af0fdacc0cbd4a58d
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-13 19:45:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(num)
-  if 
-    num == 2
+  if num <= 1
+    return false
+  elsif num == 2
     return true
   elsif (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
   else
